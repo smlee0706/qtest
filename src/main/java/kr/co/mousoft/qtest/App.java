075039c75689944d05e519679dba6ff98d9eeef2
RSH <RSH@WIN-TCLM28K5A4B>
2018-12-03 16:24:46 +0900

    change the text print message.

---
+++ src/main/java/kr/co/mousoft/qtest/App.java
@@ -0,0 +1,13 @@
+package kr.co.mousoft.qtest;
+
+/**
+ * Hello world! qTest Project
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello qTest!" );
+    }
+}
